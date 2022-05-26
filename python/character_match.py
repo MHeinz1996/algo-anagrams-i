87a140d9617a35120e7b7416e0905f073afcda1e
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-26 15:17:07 -0500

    Completed algo

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,16 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	# create 2 lists of all characters except for white spaces
+	x = [i.lower() for i in string1 if i!=' ']	
+	y = [j.lower() for j in string2 if j!=' ']
+
+	# Sort the two lists
+	sorted_x = sorted(x)
+	sorted_y = sorted(y)
+	
+	# Compare the two lists
+	if(sorted_x == sorted_y):
+		return True
+	else:
+		return False
